f14833e4d946d8921c172305d45aa84380d5a325
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2019-05-11 13:17:13 +0100

    finished user CRUD operations

--- api-routes.js
+++ api-routes.js
@@ -1,16 +1,22 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.json({
-       status: 'API is working',
-       message: 'Welcome to the Nephele API'
-    });
+  res.json({
+     status: 'API is working',
+     message: 'Welcome to the Nephele API'
+  });
 });
 
 const userController = require('./userController');
 
-console.log(userController.index);
+router.route('/users')
+  .get(userController.index)
+  .post(userController.create);
 
-router.route('/users').get(userController.index).post(userController.new);
+router.route('/users/:user_id')
+  .get(userController.view)
+  .patch(userController.update)
+  .put(userController.update)
+  .delete(userController.delete);
 
 module.exports = router;
--- userController.js
+++ userController.js
@@ -1,32 +1,75 @@
 const User = require('./userModel');
 
-// indexing 
-exports.index = (_, res) => {
-    User.find({}, {_id: 0, username: 1, password: 1, email: 1},(err, users) => {
-        if(err)
-            return res.json({
-                status: 'error',
-                message: err
-            });
-        res.json({
-            status: 'success',
-            message: 'Users retrieved successfully',
-            data: users
+const error_handler = (res, error) => {
+  return res.json({
+          status: 'error',
+          message: error
         });
-    });
 }
 
-// new user
-exports.new = (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    }, (err, user) => {
-        if (err) return res.json(err);
-        res.json({
-            message: 'New user created!',
-            data: user
-        });
+// Get all users
+exports.index = (_, res) => {
+  User.get((err, users) => {
+    if(err) return error_handler(res, err);
+    res.json({
+      status: 'success',
+      message: 'Users retrieved successfully',
+      data: users
     });
+  });
+}
+
+// View single User
+exports.view = (req, res) => {
+  User.findById(req.params.user_id, (err, user) => {
+    if(err) return error_handler(res, err);
+    res.json({
+      message: 'User details',
+      data: user
+    });
+  });
+}
+
+// Create new user
+exports.create = (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  }, (err, user) => {
+    if(err) return error_handler(res, err);
+    res.json({
+      message: 'New user created!',
+      data: user
+    });
+  });
+}
+
+// Update user information
+exports.update = (req, res) => {
+  User.findById(req.params.user_id, (err, user) => {
+    if(err) return error_handler(res, err);
+    user.username = req.body.username ? req.body.username : user.username;
+    user.password = req.body.password ? req.body.password : user.password;
+    user.email = req.body.email ? req.body.email : user.email;
+
+    user.save(err => {
+      if(err) return error_handler(res, err);
+      res.json({
+        message: 'User info updated',
+        data: user
+      });
+    });
+  });
+}
+
+// Delete user
+exports.delete = (req, res) => {
+  User.remove({_id: req.params.user_id}, (err, _) => {
+    if(err) return error_handler(res, err);
+    res.json({
+      status: 'success',
+      message: 'User deleted'
+    });
+  });
 }
